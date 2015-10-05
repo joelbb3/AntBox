671c1f5790a332881a3de30c29298d387b037579
Joel Brand <joelb94@gmail.com>
2015-10-05 22:02:00 +1100

    Grid now properly constructs triangles loaded with their neighbours and with vertices associated with their correspondent faces.

--- Ant/Ant/Grid.cpp
+++ Ant/Ant/Grid.cpp
@@ -39,6 +39,7 @@ void GridManager::makeZones() {
         for (int j = 0; j < vertexArray[i].size() - 1; ++j){
             squareColumn.push_back(std::shared_ptr<SquareFace>(new SquareFace(ID, vertexArray[i][j], vertexArray[i+1][j], vertexArray[i][j+1], vertexArray[i+1][j+1])));
         }
+		ID += 2;
         squareArray.push_back(squareColumn);
     }
     
@@ -61,49 +62,114 @@ void GridManager::makeZones() {
     }
     
     // Associate vertices with faces.
-    int 
-    squareArray[0][0].topLeft->associate(*squareArray[0][0].top->get());
-    squareArray[squareArray.size()-1][squareArr]
-    
-    
-	for (double i = 0; i < sandboxWidth; i += squareWidth) {
-		std::vector<PointSquare> squaresColumn;
-		for (double j = 0; j < sandboxHeight; j += squareHeight) {
-			// Insert into the squarePoints map the ID for the square (incrementing by two) as well as the points in the following order: top left, top right, bottom left, bottom right.
-			squaresColumn.push_back(PointSquare(ID, Vector(i, j, getDepth(i, j)), Vector(i + squareWidth, j, getDepth(i + squareWidth, j)),
-				Vector(i, j + squareHeight, getDepth(i, j + squareHeight)), Vector(i + squareWidth, j + squareHeight, getDepth(i + squareWidth, j + squareHeight))));
-			ID += 2; // Increment ID by two each time as each square will be divided into two triangles.
+	for (int i = 0; i < squareArray.size(); ++i) {
+		for (int j = 0; j < squareArray[i].size(); ++j) {
+			SquareFace& square = *squareArray[i][j].get();
+
+			// We move from left to right across each row, associating the left-hand (top-left and bottom-left) vertices appropriately:
+
+			// Every vertex is associated with the faces in its own square.
+			square.topLeft->associate(*square.top.get());
+			square.bottomLeft->associate(*square.top.get());
+			square.bottomLeft->associate(*square.bottom.get());
+
+			// If there is a square to the left.
+			if (i > 0) {
+				square.topLeft->associate(*square.left.lock()->top.get());
+				square.topLeft->associate(*square.left.lock()->bottom.get());
+				square.bottomLeft->associate(*square.left.lock()->top.get());
+			}
+			// If there is a square above.
+			if (j > 0) {
+				square.topLeft->associate(*square.up.lock()->top.get());
+				square.topLeft->associate(*square.up.lock()->bottom.get());
+				square.topRight->associate(*square.up.lock()->bottom.get());
+			}
+			// If there is a square below.
+			if (j < squareArray[i].size() - 1) {
+				square.bottomLeft->associate(*square.down.lock()->top.get());
+				square.bottomRight->associate(*square.up.lock()->top.get());
+				square.bottomRight->associate(*square.up.lock()->bottom.get());
+			}
+			// If there is a square to the diagonal top left.
+			if (i > 0 && j > 0) {
+				square.topLeft->associate(*square.up.lock()->left.lock()->bottom.get());
+			}
+			// If there is a square to the diagonal bottom left.
+			if (i > 0 && j < squareArray[i].size() - 1) {
+				square.bottomLeft->associate(*square.down.lock()->left.lock()->top.get());
+				square.bottomLeft->associate(*square.down.lock()->left.lock()->bottom.get());
+			}
+
+			// Finally, if we are at the last square in the row, we must associate the right-hand (top-right and bottom-right) vertices appropriately:
+
+			// Every vertex is associated with the faces in its own square.
+			square.topRight->associate(*square.top.get());
+			square.topRight->associate(*square.bottom.get());
+			square.bottomRight->associate(*square.bottom.get());
+
+			// If there is a square above.
+			if (j > 0) {
+				square.topRight->associate(*square.up.lock()->bottom.get());
+			}
+
+			// If there is a square below.
+			if (j < squareArray[i].size() - 1) {
+				square.bottomRight->associate(*square.down.lock()->top.get());
+				square.bottomRight->associate(*square.down.lock()->bottom.get());
+			}
 		}
-		squaresArray.push_back(squaresColumn);
 	}
-	for (int i = 0; i < squaresArray.size(); ++i) {
-		for (int j = 0; j < squaresArray[i].size(); ++j) {
+    
+	// Set neighbours (within the squares).
+	for (int i = 0; i < squareArray.size(); ++i) {
+		for (int j = 0; j < squareArray[i].size(); ++j) {
+			// Neighbours will be stored in a map where an integer k keys a list containing ID's for all squares within a radius of k (note here that 'radius' approximates boxes as circles; ie.
+			// a 'radius' of one from some square S encompasses all squares within the box immediately surrounding S; a 'radius'of two encompasses the box immediately surrounding *that* box, etc.
 			std::map<int, std::vector<int>> neighboursList;
-			for (int k = 1; k < std::max(squaresArray.size(), squaresArray[i].size()); ++k) {
-				std::vector<int> neighboursAtDistanceK;
-				for (int l = 0; l <= k * 2; ++l) {
-					if (indexInArray(squaresArray.size(), squaresArray[i].size(), i - k + l, j - k)) {
-						neighboursAtDistanceK.push_back(squaresArray[i - k + l][j - k].ID);
-						neighboursAtDistanceK.push_back(squaresArray[i - k + l][j - k].ID + 1);
+			for (int k = 1; k < std::max(squareArray.size(), squareArray[i].size()); ++k) {
+				std::vector<int> neighboursAtRadiusK;
+				for (int l = 0; l <= k * 2; ++l) { // Note that k gives the boundary of the neighbourhood region; l traverses the region.
+					// Top row in the box at radius k.
+					if (indexInArray(squareArray.size(), squareArray[i].size(), i - k + l, j - k)) {
+						neighboursAtRadiusK.push_back(squareArray[i - k + l][j - k]->ID);
+						neighboursAtRadiusK.push_back(squareArray[i - k + l][j - k]->ID + 1);
 					}
-					if (indexInArray(squaresArray.size(), squaresArray[i].size(), i - k, j - k + l)) {
-						neighboursAtDistanceK.push_back(squaresArray[i - k][j - k + l].ID);
-						neighboursAtDistanceK.push_back(squaresArray[i - k][j - k + l].ID + 1);
+					// Left column in the box at radius k.
+					if (indexInArray(squareArray.size(), squareArray[i].size(), i - k, j - k + l)) {
+						neighboursAtRadiusK.push_back(squareArray[i - k][j - k + l]->ID);
+						neighboursAtRadiusK.push_back(squareArray[i - k][j - k + l]->ID + 1);
 					}
-					if (indexInArray(squaresArray.size(), squaresArray[i].size(), i + k - l, j + k)) {
-						neighboursAtDistanceK.push_back(squaresArray[i + k - l][j + k].ID);
-						neighboursAtDistanceK.push_back(squaresArray[i + k - l][j + k].ID + 1);
+					// Bottom row in the box at radius k.
+					if (indexInArray(squareArray.size(), squareArray[i].size(), i + k - l, j + k)) {
+						neighboursAtRadiusK.push_back(squareArray[i + k - l][j + k]->ID);
+						neighboursAtRadiusK.push_back(squareArray[i + k - l][j + k]->ID + 1);
 					}
-					if (indexInArray(squaresArray.size(), squaresArray[i].size(), i + k, j + k - l)) {
-						neighboursAtDistanceK.push_back(squaresArray[i + k][j + k - l].ID);
-						neighboursAtDistanceK.push_back(squaresArray[i + k][j + k - l].ID + 1);
+					// Right column in the box at radius k.
+					if (indexInArray(squareArray.size(), squareArray[i].size(), i + k, j + k - l)) {
+						neighboursAtRadiusK.push_back(squareArray[i + k][j + k - l]->ID);
+						neighboursAtRadiusK.push_back(squareArray[i + k][j + k - l]->ID + 1);
 					}
 				}
-				neighboursList.insert(std::make_pair(k, neighboursAtDistanceK));
+				neighboursList.insert(std::make_pair(k, neighboursAtRadiusK));
 			}
-			PointSquare& square = squaresArray[i][j];
-			faceArray.push_back(TriangleFace(square.ID, square.topLeft, square.bottomLeft, square.bottomRight, neighboursList));
-			faceArray.push_back(TriangleFace(square.ID + 1, square.topLeft, square.topRight, square.bottomRight, neighboursList));
+		}
+	}
+
+	// Collect our finished triangle faces.
+	for (auto& squareColumn : squareArray) {
+		for (auto& square : squareColumn) {
+			// Use the neighbour list in each square to pass neighbours to contained triangles.
+			for (auto it = square->neighboursList.begin(); it != square->neighboursList.end(); ++it) {
+				square->top->updateNeighbours(it->first, it->second);
+				square->bottom->updateNeighbours(it->first, it->second);
+			}
+			// Pass the triangles their IDs.
+			square->top->setID(square->ID);
+			square->bottom->setID(square->ID + 1);
+			// Finally, add to the face array.
+			faceArray.push_back(std::shared_ptr<TriangleFace>(square->top));
+			faceArray.push_back(std::shared_ptr<TriangleFace>(square->bottom));
 		}
 	}
 };
--- Ant/Ant/Grid.h
+++ Ant/Ant/Grid.h
@@ -18,43 +18,46 @@ class TriangleFace {
 public:
 	TriangleFace(int ID, Vector one, Vector two, Vector three, std::map<int, std::vector<int>> neighbourList) : ID(ID), pointOne(one), pointTwo(two), pointThree(three), neighbourList(neighbourList) {};
 	void updateNeighbours(int distance, std::vector<int> neighbourIDs);
+	void setID(int x) { ID = x; };
 };
 
 class Vertex : Vector{
     std::vector<std::weak_ptr<TriangleFace>> associatedFaces;
-    Vertex(Vector position) : position(position), Vector(position){};
-    void associate(TriangleFace& face);
+public:
+    Vertex(Vector position) : Vector(position){};
+	void associate(TriangleFace& face) {};
 };
 
 class SquareFace {
 public:
 	const int ID;
     // Vertices for the square.
-    const std::shared_ptr<Vertex> const topLeft;
-    const std::shared_ptr<Vertex> const topRight;
-	const std::shared_ptr<Vertex> const bottomLeft;
-	const std::shared_ptr<Vertex> const bottomRight;
+    const std::shared_ptr<Vertex> topLeft;
+    const std::shared_ptr<Vertex> topRight;
+	const std::shared_ptr<Vertex> bottomLeft;
+	const std::shared_ptr<Vertex> bottomRight;
     // Pointers to neighbouring squares.
-    const std::weak_ptr<SquareFace> left;
-    const std::weak_ptr<SquareFace> right;
-    const std::weak_ptr<SquareFace> up;
-    const std::weak_ptr<SquareFace> down;
+    std::weak_ptr<const SquareFace> left;
+    std::weak_ptr<const SquareFace> right;
+    std::weak_ptr<const SquareFace> up;
+    std::weak_ptr<const SquareFace> down;
     // Pointers to the two triangles encompassing the square.
-    const std::shared_ptr<TriangleFace> const top;
-    const std::shared_ptr<TriangleFace> const down
+    const std::shared_ptr<TriangleFace> top;
+	const std::shared_ptr<TriangleFace> bottom;
+	std::map<int, std::vector<int>> neighboursList;
     SquareFace(int ID, std::shared_ptr<Vertex> topLeft, std::shared_ptr<Vertex> topRight, std::shared_ptr<Vertex> bottomLeft, std::shared_ptr<Vertex> bottomRight):
         ID(ID), topLeft(topLeft), topRight(topRight),
         bottomLeft(bottomLeft), bottomRight(bottomRight)
     {
         split();
     }
-    void split();
+	void split() {};
 };
 
 class GridManager {
 	int squaresPerRow;
 	int squaresPerColumn;
-	std::vector<TriangleFace> faceArray;
+	std::vector<std::shared_ptr<TriangleFace>> faceArray;
 	double sandboxWidth;
 	double sandboxHeight;
 	double getDepth(double x, double y);
